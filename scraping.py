2c112c01ef458f1b9959ad5ab65cddff3772a5e8
Frank Wang <frankwang@Franks-MacBook-Pro.local>
2020-05-17 13:21:08 -0700

    Module 10 Submission

--- scraping.py
+++ scraping.py
@@ -5,6 +5,33 @@ from splinter import Browser
 from bs4 import BeautifulSoup
 import pandas as pd
 
+def mars_news(browser):
+
+   # Visit the mars nasa news site
+   url = 'https://mars.nasa.gov/news/'
+   browser.visit(url)
+
+   # Optional delay for loading the page
+   browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+   # Convert the browser html to a soup object and then quit the browser
+   html = browser.html
+   news_soup = soup(html, 'html.parser')
+
+   slide_elem = news_soup.select_one('ul.item_list li.slide')
+   slide_elem.find("div", class_='content_title')
+
+   # Use the parent element to find the first <a> tag and save it as  `news_title`
+   news_title = slide_elem.find("div", class_='content_title').get_text()
+   news_title
+
+   # Use the parent element to find the paragraph text
+   news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+   news_p
+   
+    
+   return news_title, news_p
+
 
 executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
 browser = Browser('chrome', **executable_path)
@@ -21,15 +48,11 @@ browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
 html = browser.html
 news_soup = BeautifulSoup(html, 'html.parser')
+
 slide_elem = news_soup.select_one('ul.item_list li.slide')
-
-
 slide_elem.find("div", class_='content_title')
 
-
 news_title = slide_elem.find("div", class_='content_title').get_text()
-
-
 news_title
 
 
@@ -65,6 +88,38 @@ df
 
 
 df.to_html
+
+def mars_facts():
+    # Add try/except for error handling
+    try:
+        # Use 'read_html' to scrape the facts table into a dataframe
+        df = pd.read_html('http://space-facts.com/mars/')[0]
+
+    except BaseException:
+        return None
+    
+    # Assign columns and set index of dataframe
+    df.columns=['Description', 'Mars']
+    df.set_index('Description', inplace=True)
+    
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html()
+
+def scrape_all():
+   # Initiate headless driver for deployment
+   browser = Browser("chrome", executable_path="chromedriver", headless=True)
+
+data = {
+      "news_title": news_title,
+      "news_paragraph": news_paragraph,
+      "featured_image": featured_image(browser),
+      "facts": mars_facts(),
+      "last_modified": dt.datetime.now()
+}
+
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
 
 browser.quit()
 
